3841ad8550e8dd7f3025c84337787258ba1ab708
Suncerie Daye <sunceried@gmail.com>
2021-09-22 15:33:18 -0700

    Frontend file stucture determined

---
+++ frontend/src/home/CommonGenres.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+export default function CommonGenre() {
+  useEffect(() => {
+      //TODO: Make an API call to get 
+  }, []);
+}
---
+++ frontend/src/home/GeneralStats.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+export default function GeneralStats() {
+  useEffect(() => { //COULD USE useFetch HOOK HERE
+      //TODO: Make an API call to get 
+  }, []);
+}
